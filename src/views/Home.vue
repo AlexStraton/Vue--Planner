8bac52ee607c929167b666eed59b31ee3ebf86e2
Alex Straton <alexandrastraton1@gmail.com>
2024-08-31 12:42:56 +0100

    extracted data from jason-server using the fetch Api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">Home</div>
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <p>{{ project.title }}</p>
+      <p>{{ project.details }}</p>
+      <p>{{ project.complete }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "home",
   components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  async mounted() {
+    const data = await fetch("http://localhost:3000/projects");
+    const parsedData = await data.json();
+    console.log(parsedData);
+    this.projects = parsedData;
+  },
 };
 </script>
