fa27cf1c5e0814b27a2a3bfe66bc8b9f39452ce0
Alex Straton <alexandrastraton1@gmail.com>
2024-09-01 10:33:54 +0100

    displayed the tasks on the home page using the fetch APi and json-server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,17 @@
   <div class="home">Home</div>
   <div v-if="projects.length">
     <div v-for="project in projects" :key="project.id">
-      <p>{{ project.title }}</p>
-      <p>{{ project.details }}</p>
-      <p>{{ project.complete }}</p>
+      <SingleProject :project="project" />
     </div>
   </div>
 </template>
 
 <script>
+import SingleProject from "@/components/SingleProject.vue";
+
 export default {
   name: "home",
-  components: {},
+  components: { SingleProject },
   data() {
     return {
       projects: [],
@@ -21,7 +21,7 @@ export default {
   async mounted() {
     const data = await fetch("http://localhost:3000/projects");
     const parsedData = await data.json();
-    console.log(parsedData);
+
     this.projects = parsedData;
   },
 };
